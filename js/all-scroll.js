2aad261850096894591783216be8e9833739b1f0
Vik <VikBn>
2018-05-10 14:24:47 +0300

    first commit

---
+++ js/all-scroll.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    $(".reserved").on("click", function (event) {
+      //отменяем стандартную обработку нажатия по ссылке
+      event.preventDefault();
+      //забираем идентификатор бока с атрибута href
+      var id  = $(this).attr('href'),
+      //узнаем высоту от начала страницы до блока на который ссылается якорь
+      top = $(id).offset().top;
+
+       top = top-110;
+      //анимируем переход на расстояние - top за 1500 мс
+      $('body,html').animate({scrollTop: top}, 2000);
+    });
+
+});
---
+++ js/main.js
@@ -0,0 +1,12 @@
+
+$('#programs').on('click', function(){
+    $('.header__progarms').toggleClass('active-block');
+});
+
+$('.group__icon').hover(function(){
+    $(this.parentNode).find('.group__icon').toggleClass('group__active');
+});
+
+$(".close__banner").click(function(){
+    $(".banner").hide();
+});
